96437c1ffa848453b990877ed82d8d2b87b27b35
Scott A. Idler <scott.a.idler@gmail.com>
2023-02-19 14:46:57 -0800

    renamed format_items and its params

--- src/cli/parser2.rs
+++ src/cli/parser2.rs
@@ -45,22 +45,22 @@ where
     println!("type={} value={:#?}", std::any::type_name::<T>(), t);
 }
 
-fn decor(items: &[&str], pre: Option<&str>, mid: Option<&str>, post: Option<&str>) -> String
+fn format_items(items: &[&str], before: Option<&str>, between: Option<&str>, after: Option<&str>) -> String
 where
 {
-    //if mid is not None, then join with mid
-    //if mid is None, then join with ""
-    let mut s = match mid {
-        Some(mid) => items.join(mid),
+    //if between is not None, then join with between
+    //if between is None, then join with ""
+    let mut s = match between {
+        Some(between) => items.join(between),
         None => items.join(""),
     };
-    //if pre is not None, then prepend with pre
-    if let Some(pre) = pre {
-        s = format!("{pre}{s}");
+    //if before is not None, then prepend with before
+    if let Some(before) = before {
+        s = format!("{before}{s}");
     }
-    //if post is not None, then append with post
-    if let Some(post) = post {
-        s = format!("{s}{post}");
+    //if after is not None, then append with after
+    if let Some(after) = after {
+        s = format!("{s}{after}");
     }
     s
 }
