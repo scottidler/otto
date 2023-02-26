b69b3118e29e8695bab77386fc4d3f803b267719
Scott A. Idler <scott.a.idler@gmail.com>
2023-02-26 10:35:05 -0800

    separated println'd matches

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports, unused_variables, unused_attributes, unused_mut, dead_code)]
 
+use std::str;
 use eyre::Result;
 use otto::cli::parser::Parser;
 
@@ -7,8 +8,11 @@ fn main() -> Result<()> {
     let parser = Parser::new()?;
     //println!("parser={:#?}", parser);
     match parser.parse() {
-        Ok(matches) => {
-            println!("matches={matches:#?}");
+        Ok(matches_vec) => {
+            for matches in matches_vec.iter() {
+                println!("matches={:#?}", matches);
+                println!("{}", "*".repeat(80));
+            }
         }
         Err(error) => {
             println!("error={error:#?}");
