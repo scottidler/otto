cc7a189c15a35f611a590ed5bb42ee4897ab8f70
Scott A. Idler <scott.a.idler@gmail.com>
2023-05-15 17:56:47 -0700

    finished renaming parse2 -> parse

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(unused_imports, unused_variables, unused_attributes, unused_mut, dead_code)]
 
 use eyre::Result;
-use otto::cli::parse2::Parser;
+use otto::cli::parse::Parser;
 use otto::cli::error::SilentError;
 use std::str;
 use std::process;
